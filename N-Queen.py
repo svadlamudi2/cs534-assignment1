cec6ff72e1b0305ec7575d84b4c06c9422366531
dmql98 <duanmuqianlin@gmail.com>
2022-09-08 11:33:22 -0400

    Update N-Queen.py

--- N-Queen.py
+++ N-Queen.py
@@ -2,7 +2,7 @@
 #number of the queens and also the size of the board
 N = 5
 
-
+# create queen by x, y, and weight
 class Queen:
     def __init__(self, x, y, weight):
         self.x = x
@@ -31,9 +31,17 @@ class Board:
         self.cols = N
         self.rows = N
         self.board = [[0 for i in range(self.cols)] for j in range(self.rows)]
+        self.queens = []
 
-    def UpdateQueens(self, x, y, weight):
-        self.board[x][y] = weight
+    def AddQueen(self, queen):
+        self.queens = []
+        self.queens.append(queen)
+
+    def UpdateQueens(self, queens):
+        #self.board[x][y] = weight
+        #load the queens to the board
+        for i in range(len(queens)):
+            self.board[queens[i].x][queens[i].y] = queens[i].weight
 
     def PrintBoard(self):
         for x in self.board:  # outer loop  
@@ -41,25 +49,25 @@ class Board:
                 print(i, end = " ") # print the elements  
             print()  
 
+#class Tree:
+    #def __init__(self):
+        
 
-
-
+#****************************************************************************************
 #create the queens list
-queens = []
-
 #need a function to read from file, now I just manuelly input the queen
+queens = []
 queens.append(Queen(0,0,1))
 queens.append(Queen(1,1,2))
 queens.append(Queen(2,2,4))
 queens.append(Queen(3,3,8))
-#
+#*****************************************************************************************
 
 #create board
 board = Board(N)
+#place the queens in place
+board.UpdateQueens(queens)
 
-#load the queens to the board
-for i in range(len(queens)):
-    board.UpdateQueens(queens[i].x, queens[i].y, queens[i].weight)
 
 #print the board
 board.PrintBoard()

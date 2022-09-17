a8d5ee4761607ef2d57028a5e0096bf7bd525daa
Sai Vadlamudi <saivarun.vadlamudi@gmail.com>
2022-09-17 12:13:22 -0400

    finalized heuristic and greedy

--- main.py
+++ main.py
@@ -18,7 +18,10 @@ if not board.isSafe(board):
         newBoard = deepcopy(board.board)
         newBoard[moves[0]][moves[1]] = 0
         newBoard[moves[2]][moves[3]] = moves[4]
-        q.put((moves[4] ** 2, newBoard, 1))
+        tempBoard = Board(newBoard, 1)
+        # multiple heuristic by * 100000000 to get greedy
+        heuristic = tempBoard.findNumQueensAttacking(tempBoard)  # * 100000000
+        q.put((heuristic + (moves[4] ** 2), newBoard, 1))
         nodeCount += 1
 
     nextNode = q.get()
@@ -31,15 +34,21 @@ if not board.isSafe(board):
             newBoard = deepcopy(nextBoard.board)
             newBoard[moves[0]][moves[1]] = 0
             newBoard[moves[2]][moves[3]] = moves[4]
-            q.put((cost + moves[4] ** 2, newBoard, level + 1))
+            newBoard[moves[2]][moves[3]] = moves[4]
+            tempBoard = Board(newBoard, 1)
+            # multiple heuristic by * 100000000 to get greedy
+            heuristic = tempBoard.findNumQueensAttacking(tempBoard) # * 100000000
+            q.put((cost + heuristic + (moves[4] ** 2), newBoard, level + 1))
             nodeCount += 1
 
         nextNode = q.get()
         cost = nextNode[0]
         nextBoard = Board(nextNode[1], nextNode[2])
-        cost += nextBoard.findNumQueensAttacking(nextBoard)
+        print("printing next board with cost: ", cost)
+        nextBoard.printBoard()
 
     print("Final Board, Cost: ", cost)
     print("Final Node Count: ", nodeCount)
     print("Final Level: ", nextBoard.level)
+    print("Final Branching Factor: ", pow(nodeCount, (1/nextBoard.level)))
     nextBoard.printBoard()
